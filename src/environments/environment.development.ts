006c40738642c873f2055104e3ac2b3481d034ce
Carlos R <carlos.r@operamx.com>
2026-05-15 13:24:39 -0600

    -

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  dashboardApiConfig: {
+    url: "https://localhost:56588/api/",
+    apiKey: "QG15czNjcjN0QXAxazN5LXJ3LWQ0NWhiMDRyZC0yMDI2QA=="
+  }
+};
